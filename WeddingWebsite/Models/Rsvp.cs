f583ec0670d93ac33e72f6982ecf86a544b22bff
Ben Origas <borigas@nebraskaglobal.com>
2014-08-25 13:30:12 -0500

    Commit existing items from old repo

---
+++ WeddingWebsite/Models/Rsvp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WeddingWebsite.Models
+{
+    public class Rsvp
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Attending { get; set; }
+        public int TotalAdults { get; set; }
+        public int TotalChildren { get; set; }
+    }
+}
