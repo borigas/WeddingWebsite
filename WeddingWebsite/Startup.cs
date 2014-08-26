75d8b2a8442741afc01f9d563e93cea9f2727c98
Ben Origas <borigas@nebraskaglobal.com>
2014-08-26 10:57:49 -0500

    Added a new empty website project with auth

---
+++ WeddingWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingWebsite.Startup))]
+namespace WeddingWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
