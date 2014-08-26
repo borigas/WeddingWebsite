f32339d53c0321241411e09a4673cb6d91664ca3
Ben Origas <borigas@nebraskaglobal.com>
2014-08-26 11:29:02 -0500

    Re-scaffolded with email field

--- WeddingWebsite/Models/Rsvp.cs
+++ WeddingWebsite/Models/Rsvp.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +10,25 @@ namespace WeddingWebsite.Models
     public class Rsvp
     {
         public int Id { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public bool Attending { get; set; }
+
+        [Required]
+        [Range(0, 5)]
+        [DisplayName("Total Adults")]
         public int TotalAdults { get; set; }
+
+        [Required]
+        [Range(0, 10)]
+        [DisplayName("Total Children")]
         public int TotalChildren { get; set; }
     }
 }
