81e77ee6f97618cbef80e9274988e0270ab51238
Ben Origas <borigas@nebraskaglobal.com>
2014-09-14 23:20:10 -0500

    Fixed many of the issues caused by the vNext migration

--- WeddingWebsite/Models/IdentityModels.cs
+++ WeddingWebsite/Models/IdentityModels.cs
@@ -9,7 +9,7 @@ namespace WeddingWebsite.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-
+        public string Name { get; set; }
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
