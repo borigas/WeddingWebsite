96a513cce42ebb66f1a762cf34019e6de9135b19
Ben Origas <borigas@nebraskaglobal.com>
2014-11-28 23:27:25 -0600

    Show new photos

--- WeddingWebsite/Controllers/PhotosController.cs
+++ WeddingWebsite/Controllers/PhotosController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -11,7 +12,18 @@ namespace WeddingWebsite.Controllers
         // GET: Photos
         public ActionResult Index()
         {
-            return View();
+            var photosPath = Server.MapPath(@"~/Content/EngagementPhotos");
+            DirectoryInfo photosDirectory = new DirectoryInfo(photosPath);
+
+            var photoFiles = photosDirectory.EnumerateFiles("*.jpg")
+                // Randomly, but repeatably, sort the files
+                .OrderBy(fi => fi.Name.GetHashCode());
+
+            // Convert to a virtual path
+            string absoluteRoot = Server.MapPath("~");
+            var photoPaths = photoFiles.Select(fi => fi.FullName.Replace(absoluteRoot, "")).ToArray();
+
+            return View(photoPaths);
         }
     }
 }
