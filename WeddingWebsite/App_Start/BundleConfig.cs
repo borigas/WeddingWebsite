5d1dee0aa04b8b8e8f180f237c7e77b26851a4a8
Ben Origas <borigas@nebraskaglobal.com>
2014-12-23 12:58:33 -0600

    Use hisrc for small images

--- WeddingWebsite/App_Start/BundleConfig.cs
+++ WeddingWebsite/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace WeddingWebsite
 
             bundles.Add(new ScriptBundle("~/bundles/jssor").Include(
                       "~/Scripts/jssor.js",
-                      "~/Scripts/jssor.slider.js"));
+                      "~/Scripts/jssor.slider.js",
+                      "~/Scripts/hisrc.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
