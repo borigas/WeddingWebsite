c4d6e85bb60a17370c86ca429fa91eecaac1ccde
Ben Origas <borigas@nebraskaglobal.com>
2014-12-25 13:31:36 -0600

    Don't minify in debug mode

--- WeddingWebsite/App_Start/BundleConfig.cs
+++ WeddingWebsite/App_Start/BundleConfig.cs
@@ -36,7 +36,11 @@ namespace WeddingWebsite
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
+#if DEBUG
+            BundleTable.EnableOptimizations = false;
+#else
             BundleTable.EnableOptimizations = true;
+#endif
         }
     }
 }
